a00b4da82152d842906d2e5aeabfaa444ca0e71a
Moon-Jang <roby8502@naver.com>
2025-02-16 18:26:06 +0900

    INIT

---
+++ src/main/java/com/hhplus/precourse/PrecourseApplication.java
@@ -0,0 +1,12 @@
+package com.hhplus.precourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrecourseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hhplus/board/PrecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
